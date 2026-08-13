41cdfd2928567e915be23e7787dbf336c9b513ee
Harish-06666 <harishrmnh01@gmail.com>
2026-08-13 11:49:13 +0530

    Add files via upload

---
+++ day19/function.js
@@ -0,0 +1,628 @@
+// function series() {
+//   let s = "";
+//   for (let i = 1; i <= 20; i++) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// series();
+
+// function series(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i++) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// series(1, 20);
+
+// function series(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i++) {
+//     s += i + " ";
+//   }
+//   return s;
+// }
+// console.log(series(1, 20));
+
+// function oddSeries() {
+//   let s = "";
+//   for (let i = 1; i <= 20; i += 2) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// oddSeries();
+
+// function oddSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// oddSeries(1, 20);
+
+// function oddSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " ";
+//   }
+//   return s;
+// }
+// console.log(oddSeries(1, 20));
+
+// function evenSeries() {
+//   let s = "";
+//   for (let i = 0; i <= 20; i += 2) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// evenSeries();
+
+
+
+
+
+// function evenSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// evenSeries(0, 20);
+
+// function evenSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " ";
+//   }
+//   return s;
+// }
+// console.log(evenSeries(0, 20));
+
+// function fiveSeries() {
+//   let s = "";
+//   for (let i = 0; i <= 20; i += 5) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// fiveSeries();
+
+// function fiveSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 5) {
+//     s += i + " ";
+//   }
+//   console.log(s);
+// }
+// fiveSeries(0, 20);
+
+// function fiveSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 5) {
+//     s += i + " ";
+//   }
+//   return s;
+// }
+// console.log(fiveSeries(0, 20));
+
+// function sumSeries() {
+//   let s = "";
+//   for (let i = 1; i <= 20; i++) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// sumSeries();
+
+// function sumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i++) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// sumSeries(1, 20);
+
+// function sumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i++) {
+//     s += i + " + ";
+//   }
+//   return s;
+// }
+// console.log(sumSeries(1, 20));
+
+// function evenSumSeries() {
+//   let s = "";
+//   for (let i = 0; i <= 20; i += 2) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// evenSumSeries();
+
+// function evenSumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// evenSumSeries(0, 20);
+
+// function evenSumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " + ";
+//   }
+//   return s;
+// }
+// console.log(evenSumSeries(0, 20));
+
+// function oddSumSeries() {
+//   let s = "";
+//   for (let i = 1; i <= 20; i += 2) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// oddSumSeries();
+
+// function oddSumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// oddSumSeries(1, 20);
+
+// function oddSumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + " + ";
+//   }
+//   return s;
+// }
+// console.log(oddSumSeries(1, 20));
+
+// function fiveSumSeries() {
+//   let s = "";
+//   for (let i = 0; i <= 20; i += 5) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// fiveSumSeries();
+
+// function fiveSumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 5) {
+//     s += i + " + ";
+//   }
+//   console.log(s);
+// }
+// fiveSumSeries(0, 20);
+
+// function fiveSumSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 5) {
+//     s += i + " + ";
+//   }
+//   return s;
+// }
+// console.log(fiveSumSeries(0, 20));
+
+// function factorialSeries() {
+//   let s = "";
+//   for (let i = 1; i <= 20; i++) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   console.log(s);
+// }
+// factorialSeries();
+
+// function factorialSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i++) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   console.log(s);
+// }
+// factorialSeries(1, 20);
+
+// function factorialSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i++) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   return s;
+// }
+// console.log(factorialSeries(1, 20));
+
+// function evenFactorialSeries() {
+//   let s = "";
+//   for (let i = 0; i <= 20; i += 2) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   console.log(s);
+// }
+// evenFactorialSeries();
+
+// function evenFactorialSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   console.log(s);
+// }
+// evenFactorialSeries(0, 20);
+
+// function evenFactorialSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   return s;
+// }
+// console.log(evenFactorialSeries(0, 20));
+
+// function oddFactorialSeries() {
+//   let s = "";
+//   for (let i = 1; i <= 20; i += 2) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   console.log(s);
+// }
+// oddFactorialSeries();
+
+// function oddFactorialSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   console.log(s);
+// }
+// oddFactorialSeries(1, 20);
+
+// function oddFactorialSeries(a, b) {
+//   let s = "";
+//   for (let i = a; i <= b; i += 2) {
+//     s += i + "/" + i + "! + ";
+//   }
+//   return s;
+// }
+// console.log(oddFactorialSeries(1, 20));
+
+// function fact() {
+//   let num = 5;
+//   let f = 1;
+
+//   for (let i = num; i >= 1; i--) {
+//     f *= i;
+//   }
+//   console.log(f);
+// }
+// fact();
+
+// function fact(num) {
+//   let f = 1;
+
+//   for (let i = num; i >= 1; i--) {
+//     f *= i;
+//   }
+//   console.log(f);
+// }
+// fact(5);
+
+// function fact(num) {
+//   let f = 1;
+
+//   for (let i = num; i >= 1; i--) {
+//     f *= i;
+//   }
+//   return f;
+// }
+// console.log(fact(5));
+
+// function prime() {
+//   let n = 5;
+//   let f = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//       f++;
+//     }
+//   }
+
+//   if (f == 2) console.log("Prime");
+//   else console.log("Not Prime");
+// }
+// prime();
+
+// function prime(n) {
+//   let f = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//       f++;
+//     }
+//   }
+
+//   if (f == 2) console.log("Prime");
+//   else console.log("Not Prime");
+// }
+// prime(5);
+
+// function prime(n) {
+//   let f = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//       f++;
+//     }
+//   }
+
+//   return f == 2;
+// }
+// console.log(prime(5));
+
+// function primeSeries() {
+//   for (let n = 1; n <= 50; n++) {
+//     let f = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//       if (n % i == 0) {
+//         f++;
+//       }
+//     }
+
+//     if (f == 2) {
+//       console.log(n);
+//     }
+//   }
+// }
+// primeSeries();
+
+// function primeSeries(last) {
+//   for (let n = 1; n <= last; n++) {
+//     let f = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//       if (n % i == 0) {
+//         f++;
+//       }
+//     }
+
+//     if (f == 2) {
+//       console.log(n);
+//     }
+//   }
+// }
+// primeSeries(50);
+
+// function primeSeries(last) {
+//   let s = "";
+
+//   for (let n = 1; n <= last; n++) {
+//     let f = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//       if (n % i == 0) {
+//         f++;
+//       }
+//     }
+
+//     if (f == 2) {
+//       s += n + " ";
+//     }
+//   }
+
+//   return s;
+// }
+// console.log(primeSeries(50));
+
+
+
+// function armstrong(n) {
+//   let temp = n;
+//   let sum = 0;
+//   let r;
+
+//   while (temp > 0) {
+//     r = temp % 10;
+//     sum += r * r * r;
+//     temp = parseInt(temp / 10);
+//   }
+
+//   if (sum == n) console.log("Armstrong");
+//   else console.log("Not Armstrong");
+// }
+// armstrong(153);
+
+// function armstrong(n) {
+//   let temp = n;
+//   let sum = 0;
+//   let r;
+
+//   while (temp > 0) {
+//     r = temp % 10;
+//     sum += r * r * r;
+//     temp = parseInt(temp / 10);
+//   }
+
+//   return sum == n;
+// }
+// console.log(armstrong(153));
+
+// function armstrongSeries() {
+//   for (let n = 1; n <= 1000; n++) {
+//     let temp = n;
+//     let sum = 0;
+//     let r;
+
+//     while (temp > 0) {
+//       r = temp % 10;
+//       sum += r * r * r;
+//       temp = parseInt(temp / 10);
+//     }
+
+//     if (sum == n) console.log(n);
+//   }
+// }
+// armstrongSeries();
+
+// function armstrongSeries(last) {
+//   for (let n = 1; n <= last; n++) {
+//     let temp = n;
+//     let sum = 0;
+//     let r;
+
+//     while (temp > 0) {
+//       r = temp % 10;
+//       sum += r * r * r;
+//       temp = parseInt(temp / 10);
+//     }
+
+//     if (sum == n) console.log(n);
+//   }
+// }
+// armstrongSeries(1000);
+
+// function armstrongSeries(last) {
+//   let s = "";
+
+//   for (let n = 1; n <= last; n++) {
+//     let temp = n;
+//     let sum = 0;
+//     let r;
+
+//     while (temp > 0) {
+//       r = temp % 10;
+//       sum += r * r * r;
+//       temp = parseInt(temp / 10);
+//     }
+
+//     if (sum == n) s += n + " ";
+//   }
+
+//   return s;
+// }
+// console.log(armstrongSeries(1000));
+
+// function sumAvg() {
+//   let n = 10;
+//   let sum = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+
+//   console.log(sum);
+//   console.log(sum / n);
+// }
+// sumAvg();
+
+// function sumAvg(n) {
+//   let sum = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+
+//   console.log(sum);
+//   console.log(sum / n);
+// }
+// sumAvg(10);
+
+// function sumAvg(n) {
+//   let sum = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+
+//   return { sum, avg: sum / n };
+// }
+// console.log(sumAvg(10));
+
+// function sumDigits() {
+//   let n = 147;
+//   let sum = 0;
+
+//   while (n > 0) {
+//     let r = n % 10;
+//     sum += r;
+//     n = parseInt(n / 10);
+//   }
+
+//   console.log(sum);
+// }
+// sumDigits();
+
+// function sumDigits(n) {
+//   let sum = 0;
+
+//   while (n > 0) {
+//     let r = n % 10;
+//     sum += r;
+//     n = parseInt(n / 10);
+//   }
+
+//   console.log(sum);
+// }
+// sumDigits(147);
+
+// function sumDigits(n) {
+//   let sum = 0;
+
+//   while (n > 0) {
+//     let r = n % 10;
+//     sum += r;
+//     n = parseInt(n / 10);
+//   }
+
+//   return sum;
+// }
+// console.log(sumDigits(147));
+
+// function palindrome() {
+//   let str = "MALAYALAM";
+//   let rev = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     rev += str[i];
+//   }
+
+//   if (str == rev) console.log("Palindrome");
+//   else console.log("Not Palindrome");
+// }
+// palindrome();
+
+// function palindrome(str) {
+//   let rev = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     rev += str[i];
+//   }
+
+//   if (str == rev) console.log("Palindrome");
+//   else console.log("Not Palindrome");
+// }
+// palindrome("MALAYALAM");
+
+// function palindrome(str) {
+//   let rev = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     rev += str[i];
+//   }
+
+//   return str == rev;
+// }
+// console.log(palindrome("MALAYALAM"));    
