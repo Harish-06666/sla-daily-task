bbce61fe242918325c532a8fd2723804b6e04639
Harish-06666 <harishrmnh01@gmail.com>
2026-08-13 11:39:30 +0530

    Add files via upload

---
+++ day15/java1.js
@@ -0,0 +1,25 @@
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h2 style="background-color: maroon; color: white">What is javascript?</h2>
+    <h3>
+      Javacsript is a programming language used to make webpages interactive and
+      behavioural.
+    </h3>
+    <br />
+    <br />
+    <h2 style="background-color: maroon; color: white">
+      Add a script tag inside HTML and print 'Hello JS'.
+    </h2>
+    <script>
+      window.alert("hello");
+      console.log("Hello Js");
+      document.writeln("Hello Js");
+    </script>
+  </body>
+</html>
